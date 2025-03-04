40a35fdebfedb795e7474c3dc54c17354c2bfb68
Muhammad Umair <93200938+umair763@users.noreply.github.com>
2025-03-04 22:20:17 +0500

    issues resolved logic corrected

--- client/src/app/routes/AppRoutes.jsx
+++ client/src/app/routes/AppRoutes.jsx
@@ -1,34 +1,32 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Signup from '../pages/Auth/Signup';
 import Signin from '../pages/Auth/Signin';
 import Layout from '../layout/Layout';
-import AuthContext from '../context/AuthContext';
 import PersonalDashboard from '../pages/PersonalDashboard';
 import MainPG from '../pages/MainPG';
 import CoursePage from '../pages/CoursePage';
 import ProtectedRoutes from '../../ProtectedRoutes';
 
 function AppRoutes() {
-   const { isAuthenticated } = useContext(AuthContext);
-
    return (
       <Routes>
          {/* Public Routes */}
          <Route path="/signup" element={<Signup />} />
          <Route path="/signin" element={<Signin />} />
 
-         {/* Protected Routes */}
-         <Route element={<ProtectedRoutes />}>
-            <Route path="/" element={<Layout />}>
-               <Route index element={<MainPG />} /> 
+         {/* Main Layout Wrapper */}
+         <Route path="/" element={<Layout />}>
+            <Route index element={<MainPG />} /> {/* Default route */}
+            <Route path="courses" element={<CoursePage />} />
+            {/* Protected Routes */}
+            <Route element={<ProtectedRoutes />}>
                <Route path="dashboard" element={<PersonalDashboard />} />
-               <Route path="courses" element={<CoursePage />} />
             </Route>
          </Route>
 
-         {/* Redirect for unknown routes */}
-         <Route path="*" element={<Navigate to={isAuthenticated ? '/' : '/signin'} replace />} />
+         {/* Redirect all unknown paths to home */}
+         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
    );
 }
